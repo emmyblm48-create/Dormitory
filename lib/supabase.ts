8c6c7ed474cfe3958d613a825549fa9ebeb057f4
emmy-chatchapong <suup532@gmail.com>
2026-07-22 15:39:45 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // เปลี่ยนค่าเป็น URL และ Key โปรเจกต์ของคุณ
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://aktyghpazejsihdbvrle.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'ใส่_ANON_KEY_ของคุณที่นี่';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://aktyghpazejsihdbvrle.supabase.co/rest/v1/';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrdHlnaHBhemVqc2loZGJ2cmxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyOTExNjUsImV4cCI6MjA4NTg2NzE2NX0.xUW3xZ5RREFUSsya5Tyf6dhurRFTiCqMTx52xCte6ro';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
